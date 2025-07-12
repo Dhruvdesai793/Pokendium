dffbfa37b061dca3809ed2b9235e73391c4dda5c
Dhruvdesai407 <blixture793@gmail.com>
2025-07-12 10:23:37 +0530

    workflow added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Pokendium/' 
+  base: '/Pokendium/',
+  build: {
+    outDir: 'dist', 
+  },
 })
